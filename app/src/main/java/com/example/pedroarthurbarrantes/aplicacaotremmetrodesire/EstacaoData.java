79f20570ac71f200474bb70a4cd07bb1a5f573d0
Pedro Arthur Barrantes <pedroarthurbarrantes@Pedros-iMac.local>
2018-06-21 22:35:50 -0300

    Meu primeiro commit da aplicação

---
+++ app/src/main/java/com/example/pedroarthurbarrantes/aplicacaotremmetrodesire/EstacaoData.java
@@ -0,0 +1,22 @@
+package com.example.pedroarthurbarrantes.aplicacaotremmetrodesire;
+
+public class EstacaoData {
+
+    String nome,url;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
